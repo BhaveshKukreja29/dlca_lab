bda2763e7a3eab46e83022dc6e009af3195dd6bc
BhaveshKukreja29 <146556002+BhaveshKukreja29@users.noreply.github.com>
2024-10-16 00:25:51 +0530

    Update decimal-binary.c

--- decimal-binary.c
+++ decimal-binary.c
@@ -6,94 +6,89 @@ int power(int n, int exp);
 
 int main()
 {
-	int n;
-	int num[32];
-	int c;
-	
-	printf("Enter 1 for decimal to binary, and 2 for binary to decimal.\n");
-	printf("Enter your choice: ");
-	scanf("%d", &c);
-	
-	
-	while (c < 3)
-	{
-	switch(c)
-	{
-		case 1:
-			printf("Enter the number: ");
-			scanf("%d", &n);
-	
-			int count = binary_banao(n, num);
-			
-			printf("Binary number: ");
-			for (int i = count; i >= 0; i--)
-			{
-				printf("%d", num[i]);
-			}
+    int n;
+    int num[32];
+    int c;
 
-			
-			break;
-		case 2:
-			printf("Enter the binary number: ");
-			
-			int temp[32] = {0};
-			
-			for (int i = 0; i < 8; i++)
-			{
-				scanf("%d", &num[i]);
-			} 
-			
-			
-			for (int i = 0; i < 8; i++)
-			{
-				if (temp[i] != 0 || temp[i] != 1)
-				{
-					break;
-				}
-				
-				num[i] = temp[i];
-			}
-			
-			decimal(num);
-	}
-	}
+    do
+    {
+        printf("Enter 1 for decimal to binary, and 2 for binary to decimal.\n");
+        printf("Enter your choice: ");
+        scanf("%d", &c);
 
-	return 0;
+        switch (c)
+        {
+            case 1:
+                printf("Enter the number: ");
+                scanf("%d", &n);
+
+                int count = binary_banao(n, num);
+
+                printf("Binary number: ");
+                for (int i = count; i >= 0; i--)
+                {
+                    printf("%d", num[i]);
+                }
+                printf("\n");
+                break;
+
+            case 2:
+                printf("Enter the 8-bit binary number: ");
+                for (int i = 0; i < 8; i++)
+                {
+                    scanf("%d", &num[i]);
+                    if (num[i] != 0 && num[i] != 1)
+                    {
+                        printf("Invalid binary digit entered.\n");
+                        return 1;
+                    }
+                }
+
+                decimal(num);
+                break;
+
+            default:
+                printf("Invalid choice.\n");
+                break;
+        }
+    } while (c < 3);
+
+    return 0;
 }
 
 int binary_banao(int n, int num[])
 {
-	int count = 0;
-	
-	while (n != 0)
-	{
-		num[count] =  n % 2;
-		n = n/2;
-		count++;
-	}
-	
-	return count;
+    int count = 0;
+
+    while (n != 0)
+    {
+        num[count] = n % 2;
+        n = n / 2;
+        count++;
+    }
+
+    return count - 1;
 }
 
 void decimal(int num[])
 {
-		int sum = 0;
-		
-		for (int i = 0; i < 8; i++)
-		{
-			sum += power(2, 7 - i)*num[i];
-		}
-		
-		printf("Decimal value is: %d", sum);
+    int sum = 0;
+
+    for (int i = 0; i < 8; i++)
+    {
+        sum += power(2, 7 - i) * num[i];
+    }
+
+    printf("Decimal value is: %d\n", sum);
 }
 
 int power(int n, int exp)
 {
-	int sum = 1;
-	for (int i = 0; i < exp; i++)
-	{
-		sum *= n;
-	}
-	
-	return sum;
+    int sum = 1;
+    for (int i = 0; i < exp; i++)
+    {
+        sum *= n;
+    }
+
+    return sum;
 }
